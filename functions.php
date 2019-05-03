b960f84b5ec231ba87b3613bf585f36bd957685c
Tom Woodward <twwoodward@me.com>
2019-05-03 10:40:39 -0400

    added local acf saver

--- functions.php
+++ functions.php
@@ -253,3 +253,18 @@ function bannerMaker(){
 
     } 
 }
+
+
+
+//ACF JSON SAVER
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+    
+    // update path
+    $path = get_stylesheet_directory() . '/acf-json';
+    
+    // return
+    return $path;
+    
+}
