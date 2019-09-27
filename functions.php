af3fdaa13a022622c7766b005193a98221883235
Tom Woodward <twwoodward@me.com>
2019-09-27 13:34:20 -0400

    fix hypen vs underscore

--- functions.php
+++ functions.php
@@ -487,8 +487,8 @@ function envs_portfolio_json($response){
     $data['envs']['graduation_date'] = get_field( 'expected_graduation_date', $cv_id);
     $data['envs']['majors'] = $major;
     $data['envs']['minors'] = $minor;
-    $data['envs']['bio-pic'] = get_field('profile_picture', 'option')['sizes'];
-    $data['envs']['proj-categories'] = get_field('menu_cats', 'option');
+    $data['envs']['bio_pic'] = get_field('profile_picture', 'option')['sizes'];
+    $data['envs']['proj_categories'] = get_field('menu_cats', 'option');
     $response->set_data($data);
     return $response;
 }
